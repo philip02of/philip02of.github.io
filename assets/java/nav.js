dd1d3c310589851e80784d2589e8ed07ca3f9563
philip02of <philip02of@gmail.com>
2018-10-08 23:10:42 +0200

    Add files via upload
    1.2

--- assets/java/nav.js
+++ assets/java/nav.js
@@ -1,10 +1,19 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
   	if($(document).scrollTop() > 5) {
-    	$('#nav').addClass('shrink');
+    	$('nav').addClass('shrink');
     }
     else {
-    $('#nav').removeClass('shrink');
+    $('nav').removeClass('shrink');
     }
   });
 });
+
+function responsiveNav() {
+    var x = document.getElementById("navbar");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
